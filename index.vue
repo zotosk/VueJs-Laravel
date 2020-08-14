c38c012df398385418c57e543844e4313b4078a3
unknown <kzwtos@gmail.com>
2020-08-14 11:21:12 +0300

    .

---
+++ index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="role">
+    <div class="md-6"></div>
+    <div class="md-6t"></div>
+  </div>
+  <div class="row"></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
