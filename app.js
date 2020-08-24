a9690ceb30159d8feaac411440255da998bb1c99
unknown <kzwtos@gmail.com>
2020-08-24 16:08:01 +0300

    .

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ var app = new Vue({
   el: "#app",
   data() {
     return {
-      header: "Shopping list",
-      image: "/assets/img/tshirt-blue.jpg",
-      product_name: "T-Shirt",
-      product_description: "lorem lorem lorem"
+      app_name: "Shopping list",
+      product_image: "/assets/img/tshirt-blue.jpg",
+      product_link: "#",
+      product_name: "Under Armour Men's Tech 2.0 Short Sleeve T-Shirt",
+      product_description: "Product info",
+      onSale: true,
+      inventory: 50,
+      details: [
+        "80% cotton", "20% polyester", "Made in Greece", "Size : XXL"
+      ]
     };
   }
 });
