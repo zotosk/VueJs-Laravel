1f5b9e96fe6e89877885cf0f1d0d2b370098f552
unknown <kzwtos@gmail.com>
2020-08-25 17:01:05 +0300

    .

--- app.js
+++ app.js
@@ -6,34 +6,39 @@ var app = new Vue({
       app_name: "Shopping list",
       product_image: "/assets/img/tshirt-green.jpg",
       product_link: "#",
-      product_name: "Under Armour Men's Tech 2.0 Short Sleeve T-Shirt",
+      product_name: "Men's Tech 2.0 Short Sleeve T-Shirt",
+      product_brand: "Under Armour",
       product_code: '#00PRD34',
       product_description: "Product info",
-      onSale: true,
       inStock: true,
       inventory: 50,
+      selectedVariant: 0,
       details: [
         "80% cotton", "20% polyester", "Made in Greece", "Size : XXL"
       ],
       variants: [{
           variantId: 222,
           variantColor: "#2ecc71",
-          variantImage: "/assets/img/tshirt-green.jpg"
+          variantImage: "/assets/img/tshirt-green.jpg",
+          variantQuantity: 10
         },
         {
           variantId: 223,
           variantColor: "#3498db",
-          variantImage: "/assets/img/tshirt-blue.jpg"
+          variantImage: "/assets/img/tshirt-blue.jpg",
+          variantQuantity: 0
         },
         {
           variantId: 224,
           variantColor: "#f1c40f",
-          variantImage: "/assets/img/tshirt-yellow.jpg"
+          variantImage: "/assets/img/tshirt-yellow.jpg",
+          variantQuantity: 0
         },
         {
           variantId: "goku",
           variantColor: "#ffd194",
-          variantImage: "/assets/img/tshirt-goku.jpg"
+          variantImage: "/assets/img/tshirt-goku.jpg",
+          variantQuantity: 10
         }
       ],
       sizes: ["small", "medium", "large", "xxl"],
@@ -44,8 +49,19 @@ var app = new Vue({
     addToCart: function () {
       this.cart += 1;
     },
-    updateProduct: function (variantImage) {
-      this.product_image = variantImage
+    updateProduct: function (index) {
+      this.selectedVariant = index
     }
-  }
+  },
+  computed: {
+    title() {
+      return this.product_brand + ' ' + this.product_name
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    onSale() {
+      return this.variants[this.selectedVariant].variantQuantity
+    }
+  },
 });
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -7,29 +7,34 @@ var app = new Vue({
       app_name: "Shopping list",
       product_image: "/assets/img/tshirt-green.jpg",
       product_link: "#",
-      product_name: "Under Armour Men's Tech 2.0 Short Sleeve T-Shirt",
+      product_name: "Men's Tech 2.0 Short Sleeve T-Shirt",
+      product_brand: "Under Armour",
       product_code: '#00PRD34',
       product_description: "Product info",
-      onSale: true,
       inStock: true,
       inventory: 50,
+      selectedVariant: 0,
       details: ["80% cotton", "20% polyester", "Made in Greece", "Size : XXL"],
       variants: [{
         variantId: 222,
         variantColor: "#2ecc71",
-        variantImage: "/assets/img/tshirt-green.jpg"
+        variantImage: "/assets/img/tshirt-green.jpg",
+        variantQuantity: 10
       }, {
         variantId: 223,
         variantColor: "#3498db",
-        variantImage: "/assets/img/tshirt-blue.jpg"
+        variantImage: "/assets/img/tshirt-blue.jpg",
+        variantQuantity: 0
       }, {
         variantId: 224,
         variantColor: "#f1c40f",
-        variantImage: "/assets/img/tshirt-yellow.jpg"
+        variantImage: "/assets/img/tshirt-yellow.jpg",
+        variantQuantity: 0
       }, {
         variantId: "goku",
         variantColor: "#ffd194",
-        variantImage: "/assets/img/tshirt-goku.jpg"
+        variantImage: "/assets/img/tshirt-goku.jpg",
+        variantQuantity: 10
       }],
       sizes: ["small", "medium", "large", "xxl"],
       cart: 0
@@ -39,8 +44,19 @@ var app = new Vue({
     addToCart: function addToCart() {
       this.cart += 1;
     },
-    updateProduct: function updateProduct(variantImage) {
-      this.product_image = variantImage;
+    updateProduct: function updateProduct(index) {
+      this.selectedVariant = index;
+    }
+  },
+  computed: {
+    title: function title() {
+      return this.product_brand + ' ' + this.product_name;
+    },
+    image: function image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    onSale: function onSale() {
+      return this.variants[this.selectedVariant].variantQuantity;
     }
   }
 });
